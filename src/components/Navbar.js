22ba9cac3edf3b8eecd9baf71be2f3d3a11e4ed8
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-11-25 14:51:03 +0100

    setup nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return(
+        
+    <section id="Navbar">
+      <div className='navbar-full'>
+        <p>Ismael Lo // Astri</p>
+      </div>
+    </section>
+    )
+}
